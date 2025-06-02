d11be44f39983b27913babd1fe0d268ce0d6420d
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-06-02 23:26:45 +0530

    Update height or depth of a binary tree.java

--- height or depth of a binary tree.java
+++ height or depth of a binary tree.java
@@ -1,1 +1,21 @@
+//tree any qs =traversal like LL(O(n)) so tree's bestie=recursion, recursion has base case, self-call , base-case
+//M1:
+class Solution {
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        //M1: RECURSION
+        if(node==null)
+            return -1;
+        int left=height(node.left);
+        int right=height(node.right);
+        return Math.max(left,right)+1;
+    }
+}
+//M2:LEVEL ORDER TRAVERSAL...QUEUE
+/*
+[Expected Approach – 2] Using Level Order Traversal – O(n) Time and O(n) Space
+The idea is, if we take a closer look at the depth first traversal, we can notice that after we process the last node of the current level, the next level is completely in the queue. We use this property and insert a special NULL into the queue to indicate end of a level.
+*/
 
+//M3:[Alternate Approach] Using Level Order Traversal – O(n) Time and O(n) Space
+// This method also uses the same concept that when we process the last node of a current level, the next level is completely in the queue. Instead of adding a null in the Queue. Simply increase the counter when the level increases and push the children of current node into the queue, then remove all the nodes from the queue of the current Level.
