5fbce3f118e4a6a304bbf7fdd3c2f0e6c4f64aac
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-06-05 00:22:15 +0530

    Update Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -1,16 +1,16 @@
 Class Main
   {
-    public void reverse_string(char[] st)
+  public void reverse_string(char[] st)
     {
-      int n=st.length;
-      for(int i=0;i<n/2;i++)
+    int n=st.length;
+    for(int i=0;i<n/2;i++)
       {
-        char temp=s[i];
+        chartemp=s[i];
         s[i]=s[n-i-1];
         s[n-i-1]=temp;
       }
-    }
   }
+}
   //space complexity taking dev sir reversal technique
     //     String s1="";
     //     for(char i:s)
