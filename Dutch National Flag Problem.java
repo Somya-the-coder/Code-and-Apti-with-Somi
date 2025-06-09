88bf61f5f6642ae97273844772a05260434336de
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-06-10 00:40:29 +0530

    Update Dutch National Flag Problem.java

--- Dutch National Flag Problem.java
+++ Dutch National Flag Problem.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        int low=0,mid=0,high=arr.length-1,temp=0;
+        while(mid<=high)
+        {
+            if(arr[mid]==0)
+            {
+                temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                mid++;low++;
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+        }
+    }
+}
